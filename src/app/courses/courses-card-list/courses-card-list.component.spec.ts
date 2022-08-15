4a3b107e690f0117c69ff779d38f1762fa17518c
Michael Moore <michael@Michaels-MacBook-Pro.local>
2022-08-15 08:09:45 -0500

    Begin courses-card-list.component testing.

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
 import {COURSES} from '../../../../server/db-data';
@@ -8,14 +8,27 @@ import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
 import {Course} from '../model/course';
 import {setupCourses} from '../common/setup-test-data';
 
-
-
 describe('CoursesCardListComponent', () => {
+
+  let component: CoursesCardListComponent;
+  let fixture: ComponentFixture<CoursesCardListComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule]
+    })
+    .compileComponents()
+      .then(() => { // Need to use waitForAsync else rest of test suite is not waiting for this. Will get component undefined when asserting toBeTruthy.
+        fixture = TestBed.createComponent(CoursesCardListComponent);
+        component = fixture.componentInstance;
+      })
+  }));
 
 
   it("should create the component", () => {
 
-   pending();
+    expect(component).toBeTruthy();
+    console.log(`🚀 ~ component`, component);// Not initialized in time
 
   });
 
@@ -33,7 +46,4 @@ describe('CoursesCardListComponent', () => {
 
   });
 
-
 });
-
-
