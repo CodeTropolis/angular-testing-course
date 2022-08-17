ef1505887a7b24da110bf66bfea808b02e50a3b7
Michael D <michael@codetropolis.com>
2022-08-17 07:18:11 -0500

    Begin unit test for home.component.

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -16,7 +16,7 @@ describe('CoursesService', () => {
         httpTestingController = TestBed.inject(HttpTestingController);
     });
  
-    xit('should find all courses', () => {
+    it('should find all courses', () => {
         coursesService.findAllCourses().subscribe(courses => {
             expect(courses).toBeTruthy();
             expect(courses.length).toBe(12 ,"incorrect number of courses");
