3fdc9a178f905d3507c5016e8388ee51fde2d72e
Michael D <michael@codetropolis.com>
2022-08-17 16:05:57 -0500

    home.component.spec.

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,42 +1,52 @@
-import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, flush, flushMicrotasks, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesModule} from '../courses.module';
 import {DebugElement} from '@angular/core';
-
 import {HomeComponent} from './home.component';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {CoursesService} from '../services/courses.service';
-import {HttpClient} from '@angular/common/http';
-import {COURSES} from '../../../../server/db-data';
 import {setupCourses} from '../common/setup-test-data';
 import {By} from '@angular/platform-browser';
 import {of} from 'rxjs';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {click} from '../common/test-utils';
-
-
-
 
 describe('HomeComponent', () => {
 
   let fixture: ComponentFixture<HomeComponent>;
   let component:HomeComponent;
   let el: DebugElement;
+  let coursesService: any;
+  const beginnerCourses = setupCourses().filter(course => course.category === "BEGINNER");
 
-  beforeEach((() => {
+  beforeEach(waitForAsync(() => {
 
+    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses'])
+
+    TestBed.configureTestingModule({
+      imports:[
+        CoursesModule,
+        NoopAnimationsModule // "No operation animations module. Without this, tab group would break."
+      ],
+      // Override CoursesService w/coursesServiceSpy
+      providers: [{provide: CoursesService, useValue: coursesServiceSpy}]
+    }).compileComponents().then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+        coursesService = TestBed.inject(CoursesService);
+    })
 
   }));
 
   it("should create the component", () => {
-
     expect(component).toBeTruthy();
-
   });
 
 
   it("should display only beginner courses", () => {
-
-    pending();
+    // Happens synchronously. The mock implementation of CoursesService returns data immediately.
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+    fixture.detectChanges();
+    const tabs = el.queryAll(By.css(".mat-tab-label"));
+    expect(tabs.length).toBe(1, "Unexpected number of tabs found.");
 
   });
 
