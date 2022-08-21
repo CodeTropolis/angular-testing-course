2c6b684bcf39b5ba5a2515edcc59cbb22e54c7ea
Michael Moore <michael@Michaels-MacBook-Pro.local>
2022-08-21 07:32:19 -0500

    Lecture 15

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -3,6 +3,7 @@ import { CoursesService } from "./courses.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
 import { COURSES } from "../../../../server/db-data";
 import { Course } from "../model/course";
+import { HttpErrorResponse } from "@angular/common/http";
 
 fdescribe("CourseService", () => {
 
@@ -58,14 +59,36 @@ fdescribe("CourseService", () => {
         coursesService.saveCourse(12, changes)
             .subscribe(course => {
                 expect(course.id).toBe(12);
+                // Per Hemant in Lecture 14 Q&A:
+                expect(course.titles.description).toBe(changes.titles.description, "Data not saved.");
+
             });
         const req = httpTestingController.expectOne('/api/courses/12');
         expect(req.request.method).toEqual("PUT");
-        expect(req.request.body.titles.description).toEqual(changes.titles.description);
+        //expect(req.request.body.titles.description).toEqual(changes.titles.description);
         req.flush({
             ...COURSES[12],
             ...changes
         })
+    });
+
+    it('should give an error if save course fails.', () => {
+        const changes: Partial<Course> = {titles:{description:'Testing Course'}};
+        coursesService.saveCourse(12, changes)
+            .subscribe(()=>
+                fail('the saved course operation should have failed.'),
+                (error:HttpErrorResponse) => {
+                    expect(error.status).toBe(500);
+                
+            });
+        // Withoug const req = ... will get: Expected no open requests, found 1: PUT /api/courses/12
+        const req = httpTestingController.expectOne('/api/courses/12');
+        expect(req.request.method).toEqual("PUT");
+        // This fails... We should get a "successful failure".
+           // In subscribe block, removed {} and used .subscribe syntax as shwon.
+        // Test now passes.
+        req.flush('Save course failed', {status: 500,
+            statusText:"Internal server error"});
     });
 
     afterEach(() => {
